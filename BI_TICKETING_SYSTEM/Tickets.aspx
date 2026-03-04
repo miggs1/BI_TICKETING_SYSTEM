e2fa5356a030cde05e04cdb99b1ff5b1c08a1516
gabkiara <gabrielkm0111@gmail.com>
2026-03-04 11:07:41 +0800

    Added Tickets page, connected to database and configured DevExpress

---
+++ BI_TICKETING_SYSTEM/Tickets.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Register Assembly="DevExpress.Web.v25.2"
+    Namespace="DevExpress.Web"
+    TagPrefix="dx" %>
+
+<%@ Page Title="Tickets" Language="C#" 
+    MasterPageFile="~/Site.Master" 
+    AutoEventWireup="true" 
+    CodeBehind="Tickets.aspx.cs" 
+    Inherits="BI_TICKETING_SYSTEM.Tickets" %>
+
+<asp:Content ID="Content1" 
+    ContentPlaceHolderID="MainContent" 
+    runat="server">
+
+    <dx:ASPxGridView ID="gvTickets" 
+        runat="server"
+        AutoGenerateColumns="False"
+        KeyFieldName="TICKET_ID"
+        OnRowUpdating="gvTickets_RowUpdating"
+        OnRowInserting="gvTickets_RowInserting"
+        OnRowDeleting="gvTickets_RowDeleting">
+
+        <Columns>
+            <dx:GridViewDataTextColumn FieldName="TICKET_NUMBER" Caption="Ticket #" />
+            <dx:GridViewDataTextColumn FieldName="TITLE" Caption="Title" />
+            <dx:GridViewDataTextColumn FieldName="PRIORITY" Caption="Priority" />
+            <dx:GridViewDataTextColumn FieldName="STATUS" Caption="Status" />
+            <dx:GridViewDataDateColumn FieldName="CREATED_AT" Caption="Created" />
+
+            <dx:GridViewCommandColumn ShowEditButton="true" ShowDeleteButton="true" />
+        </Columns>
+
+    </dx:ASPxGridView>
+
+</asp:Content>
---
+++ BI_TICKETING_SYSTEM/Tickets.aspx.cs
@@ -0,0 +1,111 @@
+﻿using BI_TICKETING_SYSTEM;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Configuration;
+using System.Data;
+
+namespace BI_TICKETING_SYSTEM
+{
+    public partial class Tickets : System.Web.UI.Page
+    {
+        private string connString;
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            var cs = ConfigurationManager.ConnectionStrings["OracleDbConnection"];
+            if (cs == null || string.IsNullOrWhiteSpace(cs.ConnectionString))
+                throw new ConfigurationErrorsException("Missing connection string 'OracleDbConnection' in web.config.");
+            connString = cs.ConnectionString;
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadTickets();
+            }
+        }
+
+        private void LoadTickets()
+        {
+            using (OracleConnection conn = new OracleConnection(connString))
+            {
+                string sql = "SELECT * FROM TICKETS ORDER BY CREATED_AT DESC";
+
+                OracleDataAdapter da = new OracleDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvTickets.DataSource = dt;
+                gvTickets.DataBind();
+            }
+        }
+
+        protected void gvTickets_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e)
+        {
+            using (OracleConnection conn = new OracleConnection(connString))
+            {
+                conn.Open();
+
+                string sql = @"UPDATE TICKETS 
+                           SET TITLE=:title,
+                               PRIORITY=:priority,
+                               STATUS=:status,
+                               UPDATED_AT=SYSDATE
+                           WHERE TICKET_ID=:id";
+
+                OracleCommand cmd = new OracleCommand(sql, conn);
+
+                cmd.Parameters.Add(":title", e.NewValues["TITLE"]);
+                cmd.Parameters.Add(":priority", e.NewValues["PRIORITY"]);
+                cmd.Parameters.Add(":status", e.NewValues["STATUS"]);
+                cmd.Parameters.Add(":id", e.Keys["TICKET_ID"]);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            e.Cancel = true;
+            gvTickets.CancelEdit();
+            LoadTickets();
+        }
+
+        protected void gvTickets_RowDeleting(object sender, DevExpress.Web.Data.ASPxDataDeletingEventArgs e)
+        {
+            using (OracleConnection conn = new OracleConnection(connString))
+            {
+                conn.Open();
+
+                string sql = "DELETE FROM TICKETS WHERE TICKET_ID=:id";
+                OracleCommand cmd = new OracleCommand(sql, conn);
+                cmd.Parameters.Add(":id", e.Keys["TICKET_ID"]);
+                cmd.ExecuteNonQuery();
+            }
+
+            e.Cancel = true;
+            LoadTickets();
+        }
+
+        protected void gvTickets_RowInserting(object sender, DevExpress.Web.Data.ASPxDataInsertingEventArgs e)
+        {
+            using (OracleConnection conn = new OracleConnection(connString))
+            {
+                conn.Open();
+
+                string sql = @"INSERT INTO TICKETS
+                          (TICKET_ID, TITLE, PRIORITY, STATUS, CREATED_AT)
+                          VALUES (TICKET_SEQ.NEXTVAL, :title, :priority, 'New', SYSDATE)";
+
+                OracleCommand cmd = new OracleCommand(sql, conn);
+                cmd.Parameters.Add(":title", e.NewValues["TITLE"]);
+                cmd.Parameters.Add(":priority", e.NewValues["PRIORITY"]);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            e.Cancel = true;
+            gvTickets.CancelEdit();
+            LoadTickets();
+        }
+    }
+}
