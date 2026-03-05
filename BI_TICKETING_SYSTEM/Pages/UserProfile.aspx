0bdc1a260d821de76494f3a1de68a4a459db3667
gabkiara <gabrielkm0111@gmail.com>
2026-03-05 15:08:01 +0800

    Created UserProfile page

---
+++ BI_TICKETING_SYSTEM/Pages/UserProfile.aspx
@@ -0,0 +1,122 @@
+﻿<%@ Page Title="My Profile" Language="C#" MasterPageFile="~/Site.Master"
+AutoEventWireup="true" CodeBehind="UserProfile.aspx.cs"
+Inherits="BI_TICKETING_SYSTEM.Pages.UserProfile" %>
+
+<asp:Content ID="MainContent" ContentPlaceHolderID="MainContent" runat="server">
+
+<div class="container-fluid">
+
+<h4 class="mb-4 font-weight-bold">My Profile</h4>
+
+<!-- Success Message -->
+<asp:Panel ID="pnlSuccess" runat="server" Visible="false" CssClass="alert alert-success">
+    <asp:Label ID="lblSuccess" runat="server"></asp:Label>
+</asp:Panel>
+
+<!-- Error Message -->
+<asp:Panel ID="pnlError" runat="server" Visible="false" CssClass="alert alert-danger">
+    <asp:Label ID="lblError" runat="server"></asp:Label>
+</asp:Panel>
+
+
+<div class="card shadow-sm">
+
+<div class="card-body">
+
+<div class="row">
+
+<div class="col-md-6">
+
+<label class="form-label">Full Name</label>
+<asp:TextBox ID="txtFullName" runat="server" CssClass="form-control" ReadOnly="true" />
+
+</div>
+
+
+<div class="col-md-6">
+
+<label class="form-label">Email</label>
+<asp:TextBox ID="txtEmail" runat="server" CssClass="form-control" ReadOnly="true" />
+
+</div>
+
+</div>
+
+<div class="row mt-3">
+
+<div class="col-md-6">
+
+<label class="form-label">Role</label>
+<asp:TextBox ID="txtRole" runat="server" CssClass="form-control" ReadOnly="true" />
+
+</div>
+
+</div>
+
+
+<div class="mt-4">
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#passwordModal">
+    Change Password
+</button>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<!-- CHANGE PASSWORD MODAL -->
+
+<div class="modal fade" id="passwordModal">
+
+<div class="modal-dialog">
+
+<div class="modal-content">
+
+<div class="modal-header bg-primary text-white">
+
+<h5 class="modal-title">Change Password</h5>
+
+</div>
+
+
+<div class="modal-body">
+
+<div class="form-group">
+
+<label>New Password</label>
+
+<asp:TextBox
+    ID="txtNewPassword"
+    runat="server"
+    CssClass="form-control"
+    TextMode="Password"
+    read-only="True">
+</asp:TextBox>
+
+</div>
+
+</div>
+
+
+<div class="modal-footer">
+
+<asp:Button
+    ID="btnSavePassword"
+    runat="server"
+    Text="Update Password"
+    CssClass="btn btn-success"
+    OnClick="btnSavePassword_Click"/>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</asp:Content>
---
+++ BI_TICKETING_SYSTEM/Pages/UserProfile.aspx.cs
@@ -0,0 +1,94 @@
+﻿using BI_TICKETING_SYSTEM.Helpers;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Web.UI;
+
+namespace BI_TICKETING_SYSTEM.Pages
+{
+    public partial class UserProfile : System.Web.UI.Page
+    {
+
+        private int CurrentUserID => Convert.ToInt32(Session["UserID"] ?? 0);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserID"] == null)
+            {
+                Response.Redirect("~/Login.aspx");
+                return;
+            }
+
+            if (!IsPostBack)
+            {
+                LoadUserProfile();
+            }
+        }
+
+
+        private void LoadUserProfile()
+        {
+            using (OracleConnection conn = DatabaseHelper.GetConnection())
+            {
+                conn.Open();
+
+                string sql = @"SELECT FULL_NAME, EMAIL, ROLE
+                               FROM BI_OJT.USERS
+                               WHERE USER_ID = :userId";
+
+                OracleCommand cmd = new OracleCommand(sql, conn);
+                cmd.Parameters.Add("userId", OracleDbType.Int32).Value = CurrentUserID;
+
+                OracleDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    txtFullName.Text = reader["FULL_NAME"].ToString();
+                    txtEmail.Text = reader["EMAIL"].ToString();
+                    txtRole.Text = reader["ROLE"].ToString();
+                }
+            }
+        }
+
+
+        protected void btnOpenPassword_Click(object sender, EventArgs e)
+        {
+            ScriptManager.RegisterStartupScript(this, GetType(),
+                "showModal", "$('#passwordModal').modal('show');", true);
+        }
+
+
+
+        protected void btnSavePassword_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string hashedPassword = PasswordHelper.HashPassword(txtNewPassword.Text);
+
+                using (OracleConnection conn = DatabaseHelper.GetConnection())
+                {
+                    conn.Open();
+
+                    string sql = @"UPDATE BI_OJT.USERS
+                                   SET PASSWORD = :password
+                                   WHERE USER_ID = :userId";
+
+                    OracleCommand cmd = new OracleCommand(sql, conn);
+
+                    cmd.Parameters.Add("password", OracleDbType.Varchar2).Value = hashedPassword;
+                    cmd.Parameters.Add("userId", OracleDbType.Int32).Value = CurrentUserID;
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                pnlSuccess.Visible = true;
+                lblSuccess.Text = "Password updated successfully.";
+
+            }
+            catch (Exception ex)
+            {
+                pnlError.Visible = true;
+                lblError.Text = "Error updating password: " + ex.Message;
+            }
+        }
+    }
+}
